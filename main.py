7c4ab9ef09a95639047fcfc9ce273b57fbc8fb9d
alaingilbert <alain.gilbert.15@gmail.com>
2012-05-27 23:19:49 -0400

    Fix port

--- main.py
+++ main.py
@@ -45,5 +45,5 @@ application = tornado.web.Application([
 
 if __name__ == "__main__":
    http_server = tornado.httpserver.HTTPServer(application)
-   http_server.listen(1337)
+   http_server.listen(8888)
    tornado.ioloop.IOLoop.instance().start()
